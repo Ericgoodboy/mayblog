d2a26526fe98067a700503631f22f5ef3f7c9af1
huangqi <huangqi@thunisoft.com>
2019-09-02 09:00:20 +0800

    add

---
+++ src/components/footer/footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="v-footer">
+        Ericgoodboy@github.io
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"v-footer"
+    }
+</script>
+
+<style scoped>
+    .v-footer{
+        /* position: absolute;
+        bottom:-50px; */
+        font-size: 15px;
+        height: 40px;
+        line-height: 40px;
+        width: 100%;
+        text-align: center;
+        margin-top:12px;
+        background: #fafafa;
+        cursor: pointer;
+    }
+</style>
